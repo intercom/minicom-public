06f668eb423903fdda341f5d0b7b420a5075e70d
Pat O'Callaghan <pat@intercom.io>
2020-09-16 09:55:10 +0100

    Update start scripts

--- script/bar/start.bat
+++ script/bar/start.bat
@@ -1,9 +1,10 @@
 @echo off
 
+setlocal
 set WWW_ROOT="./bar-website"
 set LISTEN_HOST="127.0.0.1"
 set LISTEN_PORT=8008
 
 echo "Serving %WWW_ROOT% on http://%LISTEN_HOST%:%LISTEN_PORT% (^C to stop)."
 
-ruby -run -e httpd %WWW_ROOT% -p %LISTEN_PORT% || php -S "%LISTEN_HOST%:%LISTEN_PORT%" -t %WWW_ROOT%
+ruby -run -e httpd %WWW_ROOT% -p %LISTEN_PORT% || php -S "%LISTEN_HOST%:%LISTEN_PORT%" -t %WWW_ROOT% || npx serve %WWW_ROOT% -l %LISTEN_PORT% || cd %WWW_ROOT% && python3 -m http.server %LISTEN_PORT%
