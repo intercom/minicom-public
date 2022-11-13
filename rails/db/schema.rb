d15baf4dd4d5294ea8b1bc17bfdf9e6e520567f9
cxspxr <ilovewindows96@gmail.com>
2022-11-13 19:15:01 +0100

    feat!: migrate rails 5 to rails 6

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -2,13 +2,29 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2014_03_20_225123) do
+ActiveRecord::Schema.define(version: 2022_11_13_174806) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "content", null: false
+    t.boolean "read", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
